82a662ecae50fe2b96f95c96224cee4f421d4cd4
Martín <martingomval@gmail.com>
2020-03-05 11:16:47 +0100

    Creación de Funciones Anónimas con lambda

--- Functions1stLevel.py
+++ Functions1stLevel.py
@@ -4,6 +4,9 @@ def normal(x):
 def elevated(y):
     return y * y
 
+def cube(x):
+    return x**3
+
 def addAll(limitTo, f):
     result = 0
     for i in range(limitTo+1):
@@ -11,5 +14,10 @@ def addAll(limitTo, f):
         
     return result
 
-print(addAll(100, normal))
-print(addAll(3, elevated))
+if __name__ == '__main__': #Para que cuando lo importe no se me haga todo el programa, sinó sólo lo que importo.
+    
+    print(addAll(100, normal))
+    print(addAll(3, elevated))
+    
+else:
+    print(__name__)
---
+++ FunctionsLambda.py
@@ -0,0 +1,5 @@
+from Functions1stLevel import addAll
+
+double = lambda x: x*2 #Función anónima (lambda) que la puedes poner como paramtero o crearla sin 'def'
+
+print(addAll(3, double))
